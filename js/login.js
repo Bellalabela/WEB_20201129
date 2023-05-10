ab7ef8f1175632014951c110280188a538b7dfad
leel <meat3630@naver.com>
2023-05-10 02:32:39 +0000

    10주차..

--- js/login.js
+++ js/login.js
@@ -1,14 +1,15 @@
-function login(){
+	function login(){ // 로그인
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
     let password = document.querySelector("#floatingPassword");
+    let check = document.querySelector("#idSaveCheck");
     
-    form.action = "../logout/index_login.html";
+    form.action = "../index_login.html";
     form.method = "get";
     
     if(id.value.length === 0 || password.value.length === 0){
         alert("아이디와 비밀번호를 모두 입력해주세요.");
-    }else{
+	}else{
         form.submit();
     }
 }
@@ -16,3 +17,52 @@ function login(){
 function logout(){
     location.href='../index.html';
 }
+
+
+	function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                
+                if (cookie_name[0] == "id") {
+                    return cookie_name[1];
+                }
+            }
+        }
+        return;
+}
+
+ 
+
+	
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    let id = document.querySelector("#floatingInput");
+    let check = document.querySelector("#idSaveCheck");
+    let get_id = getCookie("id");
+		
+		if(get_id) { 
+    id.value = get_id; 
+    check.checked = true; 
+    }
+		
+		if(check.checked == true) { // 아이디 체크 o
+            alert("쿠키를 저장합니다.");
+            setCookie("id", id.value, 1); // 1일 저장
+            alert("쿠키 값 :" + id.value);
+        } 
+    else { // 아이디 체크 x
+            setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
+    }
+    
+    
+}
+
+function deleteCookie(cookieName){
+    var expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() - 1);
+    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+}
+
